28e07add4f1dd10741d5353fb59f35532414be24
Joseph O'Callaghan <josephocall@live.ie>
2020-02-19 16:10:47 +0000

    Initial Spring Boot Shell

---
+++ src/test/java/com/josephocallaghan/lottoapp/LottoAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.josephocallaghan.lottoapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LottoAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
