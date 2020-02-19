cea5b7cb79166e623c32deb2a036d99b04c0d9b2
Joseph O'Callaghan <joseph.ocallaghan@clientsolutions.ie>
2020-02-19 17:23:48 +0000

    Updating Spring Maven Depenencies, adding base controller file and JUnit Test file

--- src/main/java/com/josephocallaghan/lottoapp/LottoAppApplication.java
+++ src/main/java/com/josephocallaghan/lottoapp/LottoAppApplication.java
@@ -1,13 +1,34 @@
 package com.josephocallaghan.lottoapp;
 
+import java.util.Arrays;
+
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class LottoAppApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(LottoAppApplication.class, args);
+	}
+	
+	@Bean
+	public CommandLineRunner commandLineRunner(ApplicationContext ctx) {
+		return args -> {
+
+			System.out.println("Print out beans provided by Spring Boot:");
+
+			String[] beanNames = ctx.getBeanDefinitionNames();
+			Arrays.sort(beanNames);
+			
+			for (String beanName : beanNames) {
+				System.out.println(beanName);
+			}
+
+		};
 	}
 
 }
