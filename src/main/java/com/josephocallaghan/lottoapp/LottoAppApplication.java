ac5460099f61e9ca06a60483d3a302bb0984c4b7
Joseph O'Callaghan <joseph.ocallaghan@clientsolutions.ie>
2020-02-19 17:44:30 +0000

    Added new Maven dependencies for tomcat deployments

--- src/main/java/com/josephocallaghan/lottoapp/LottoAppApplication.java
+++ src/main/java/com/josephocallaghan/lottoapp/LottoAppApplication.java
@@ -5,11 +5,13 @@ import java.util.Arrays;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
-public class LottoAppApplication {
+public class LottoAppApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(LottoAppApplication.class, args);
@@ -29,6 +31,11 @@ public class LottoAppApplication {
 			}
 
 		};
+	}
+	
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+		return builder.sources(LottoAppApplication.class);
 	}
 
 }
